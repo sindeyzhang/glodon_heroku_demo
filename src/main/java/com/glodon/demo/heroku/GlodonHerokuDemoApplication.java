b3f2c4cbbd967fbc02fbb341d0ef9e0dd4aadefa
sidney.zhang <681155@qq.com>
2022-05-04 02:07:26 +0800

    init commit

---
+++ src/main/java/com/glodon/demo/heroku/GlodonHerokuDemoApplication.java
@@ -0,0 +1,13 @@
+package com.glodon.demo.heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlodonHerokuDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlodonHerokuDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glodon/demo/heroku/GlodonHerokuDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glodon.demo.heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlodonHerokuDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
